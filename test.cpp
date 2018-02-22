a810337b8f3802942ab703040d6a3064d4a9a185
entropy82 <froutopotos.alex@gmail.com>
2018-02-22 18:31:13 +0200

    friend promt

--- test.cpp
+++ test.cpp
@@ -19,11 +19,35 @@ inline void keep_window_open()
 
 int main(void) {
 
-    char x = 'x';
-    int y = x; // convertion ascii 
-    
+    string name = "????";
 
-    cout << y << endl; 
+    cout << "enter your first name: ";
+    cin >> name;
+
+    name.resize(name.size() -1);
+
+
+    cout << "Hello " << name << endl;
+    cout << "How are you? " << endl;
+
+    cout << "Me i am fine. I miss you" << endl;
+
+    string friend_name = "???";
+    cout << "enter the friend's name: ";
+    cin >> friend_name;
+    cout << "Have you seen " << friend_name << " lately?";
+
+    char friend_sex = 0;
+
+    cout << "if the friend is male enter m or f for female: ";
+    cin >> friend_sex;
+
+    if (friend_sex =='m') {
+        cout << "tell him i called." <<endl;
+    }
+    else if (friend_sex == 'f') {
+        cout << "tell her i called\n";
+    }
 
 
 

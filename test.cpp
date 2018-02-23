d15929424af28d8737ee807fe45d19ca75f4ec12
entropy82 <froutopotos.alex@gmail.com>
2018-02-23 17:42:36 +0200

    ex1

--- test.cpp
+++ test.cpp
@@ -19,54 +19,15 @@ inline void keep_window_open()
 
 int main(void) {
 
-    string name = "????";
+    cout << "enter velocity in miles: \n";
+    float miles;
+    cin >> miles;
+    float km;
+    km = miles * 1.604;
 
-    cout << "enter your first name: ";
-    cin >> name;
-
-    name.resize(name.size() -1);
+    cout << "the velocity in km is: " << km << endl;
 
 
-    cout << "Hello " << name << endl;
-    cout << "How are you? " << endl;
-
-    cout << "Me i am fine. I miss you" << endl;
-
-    string friend_name = "???";
-    cout << "enter the friend's name: ";
-    cin >> friend_name;
-    cout << "Have you seen " << friend_name << " lately?";
-
-    char friend_sex = 0;
-
-    cout << "if the friend is male enter m or f for female: ";
-    cin >> friend_sex;
-
-    if (friend_sex =='m') {
-        cout << "tell him i called." <<endl;
-    }
-    else if (friend_sex == 'f') {
-        cout << "tell her i called\n";
-    }
-    int age = -1;
-    cout << "enter the age of the recipient: \n";
-    cin >> age;
-    if ((age <= 0) | ( age >= 101 )) {
-        cout << "you re kidding..\n"; 
-    }
-    if (age < 12) {
-        ++age;
-        cout << "next year you will be: " << age << endl;
-    }
-    if (age == 17) {
-        cout << "next year you will be able to vote" << endl;
-    }
-    if (age > 70) {
-        cout << "i hope you are enjoying retirement" << endl;
-    }
-    cout << "Yours sincerely\n" << endl << endl;
-    string my_name = "froutopoios";
-    cout << my_name << endl; 
 
 
 

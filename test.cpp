bea090af80a67381463aab083979f0289a42150f
entropy82 <froutopotos.alex@gmail.com>
2018-02-27 09:55:17 +0200

    init

--- test.cpp
+++ test.cpp
@@ -19,13 +19,30 @@ inline void keep_window_open()
 
 int main(void) {
 
-    cout << "enter velocity in miles: \n";
-    float miles;
-    cin >> miles;
-    float km;
-    km = miles * 1.604;
+    int val1, val2;
+    cout << "enter 2 values: " << endl;
 
-    cout << "the velocity in km is: " << km << endl;
+    cin >> val1 >> val2;
+
+    cout << val1 << " " << val2 << endl;
+
+    if (val1 < val2) {
+        cout << "num 2 is bigger\n" << endl;
+    }
+    else if (val1 > val2) {
+        cout << "num 1 is bigger\n" << endl;
+    }else {
+        cout << "nums are same\n" << endl;
+    }
+
+    cout << "sum is: " << val1 + val2 << endl << endl;
+
+    cout << "dif is: " << val1 - val2 << endl << endl;
+    cout << "product is: " << val1 * val2 << endl << endl;
+    cout << "ratio is: " << val1 / val2 << endl << endl;
+
+
+ 
 
 
 
